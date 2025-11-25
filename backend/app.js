b7b7ac2d0e8ee3c3558089628ee87db82eecd8a5
calibrixadv <andrei.iordache21@yahoo.com>
2025-11-25 04:38:41 +0200

    fara norm port 2

--- backend/app.js
+++ backend/app.js
@@ -13,10 +13,7 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/auth', authRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log('MongoDB connected');
     app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 }).catch(err => console.error(err));
