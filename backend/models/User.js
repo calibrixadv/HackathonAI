d20830bd4c1e5ad57dd1a98de4abaf4a53cdc026
calibrixadv <andrei.iordache21@yahoo.com>
2025-11-25 03:56:53 +0200

    update next e o functie acum

--- backend/models/User.js
+++ backend/models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function() {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 12);
     next();
